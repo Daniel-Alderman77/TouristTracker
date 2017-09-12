bbae814ed2e7b9f29e9cfcdb4dba0b18bc3e02f3
Daniel Alderman <daniel.alderman17@gmail.com>
2017-09-12 20:05:55 +0100

    Initial Commit

---
+++ TouristTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TouristTracker
+//
+//  Created by Daniel Alderman on 12/09/2017.
+//  Copyright © 2017 Daniel Alderman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
